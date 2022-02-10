aa6ba02be6d536348a53706c056c2073cfa5c4df
İbrahim karabay <46526531+karabayibrahim@users.noreply.github.com>
2022-02-11 00:40:02 +0300

    money

---
+++ Shuffle/Assets/Script/Gate/Gate.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class Gate : MonoBehaviour,ICollectable
+{
+    public TextMeshPro MyText;
+    public int MyCount;
+    public void DoCollect(GameObject _object)
+    {
+        if (gameObject.tag=="Positive")
+        {
+            if (_object.tag == "Left")
+            {
+                _object.GetComponent<Hand>().SpawnChip(MyCount);
+            }
+            else if(_object.tag == "Right")
+            {
+                Debug.Log("Sağ");
+                _object.GetComponent<Hand>().SpawnChip(MyCount);
+            }
+        }
+        else
+        {
+            if (_object.tag == "Left")
+            {
+                _object.GetComponent<Hand>().RemoveChip(MyCount);
+            }
+            else
+            {
+                _object.GetComponent<Hand>().RemoveChip(MyCount);
+            }
+        }
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (gameObject.tag=="Positive")
+        {
+            MyText.text = "+" + MyCount.ToString();
+        }
+        else
+        {
+            MyText.text = "-" + MyCount.ToString();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Shuffle/Assets/Script/Hands/Hand.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using TMPro;
+public class Hand : MonoBehaviour
+{
+    public GameObject OtherHand;
+    public List<Money> MyMoneys = new List<Money>();
+    public int SpawnCount;
+    public TextMeshPro MyMoneyText;
+
+    private int Carpan = 5;
+    public float Height;
+    public List<GameObject> Chips = new List<GameObject>();
+    public Animator Anim;
+    public Sequence seq;
+    private bool _canMove = false;
+    private float fireRate = 0.05f;
+    private float lastShot = 0;
+    void Start()
+    {
+        Anim = GetComponent<Animator>();
+        SpawnSystem();
+        foreach (var item in gameObject.GetComponentsInChildren<Money>())
+        {
+            MyMoneys.Add(item);
+        }
+        MoneyTextWrite();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SwipeMove();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<ICollectable>() != null)
+        {
+            other.gameObject.GetComponent<ICollectable>().DoCollect(gameObject);
+        }
+    }
+
+    private void SwipeMove()
+    {
+        if (Input.GetKey(KeyCode.Space)&&gameObject.tag=="Right")
+        {
+            if (MyMoneys.Count > 0 && !_canMove && Time.time > fireRate + lastShot)
+            {
+                //_canMove = true;
+                OtherHand.GetComponent<Hand>().seq.Kill(true);
+                ResetScale();
+                lastShot = Time.time;
+                Anim.CrossFade("Event", 0.05f);
+                var moveObject = MyMoneys[MyMoneys.Count - 1];
+                seq = DOTween.Sequence();
+                OtherHand.GetComponent<Hand>().MyMoneys.Add(moveObject);
+                MyMoneys.Remove(moveObject);
+                Vector3 myPoz = new Vector3(-0.5f, ((OtherHand.GetComponent<Hand>().MyMoneys.Count - 1) + Carpan) * Height, 5f);
+                moveObject.transform.SetParent(OtherHand.transform);
+                seq.Join(moveObject.transform.DOLocalJump(myPoz, -20f, 1, 0.25f)).SetEase(Ease.OutCubic)
+                   .Join(moveObject.transform.DOLocalRotate(new Vector3(90, -180, -180), 0.25f)).OnComplete(() =>
+                   {
+                       moveObject.transform.DOScale(new Vector3(moveObject.transform.localScale.x * 1.2f, moveObject.transform.localScale.y * 1.2f, moveObject.transform.localScale.z), 0.1f).From().OnComplete(() => _canMove = false);
+                   });
+                OtherHand.GetComponent<Hand>().MoneyTextWrite();
+                MoneyTextWrite();
+            }
+
+        }
+        if (Input.GetKey(KeyCode.Return) && gameObject.tag == "Left")
+        {
+            if (MyMoneys.Count > 0 && !_canMove && Time.time > fireRate + lastShot)
+            {
+                //_canMove = true;
+                OtherHand.GetComponent<Hand>().seq.Kill(true);
+                ResetScale();
+                lastShot = Time.time;
+                Anim.CrossFade("Event", 0.05f);
+                var moveObject = MyMoneys[MyMoneys.Count - 1];
+                seq = DOTween.Sequence();
+                OtherHand.GetComponent<Hand>().MyMoneys.Add(moveObject);
+                MyMoneys.Remove(moveObject);
+                Vector3 myPoz = new Vector3(-0.5f, ((OtherHand.GetComponent<Hand>().MyMoneys.Count - 1) + Carpan) * Height, 5f);
+                moveObject.transform.SetParent(OtherHand.transform);
+                seq.Join(moveObject.transform.DOLocalJump(myPoz, -20f, 1, 0.25f)).SetEase(Ease.OutCubic)
+                   .Join(moveObject.transform.DOLocalRotate(new Vector3(90, -180, -180), 0.25f)).OnComplete(() =>
+                   {
+                       moveObject.transform.DOScale(new Vector3(moveObject.transform.localScale.x * 1.2f, moveObject.transform.localScale.y * 1.2f, moveObject.transform.localScale.z), 0.1f).From().OnComplete(() => _canMove = false);
+                   });
+                OtherHand.GetComponent<Hand>().MoneyTextWrite();
+                MoneyTextWrite();
+            }
+        }
+    }
+    private void SpawnSystem()
+    {
+        for (int i = 0; i < SpawnCount; i++)
+        {
+            if (i == 0)
+            {
+                var rndChip = Random.Range(0, Chips.Count);
+                var newChip = Instantiate(Chips[rndChip], transform.position, Quaternion.identity, transform);
+                newChip.transform.localPosition = new Vector3(-0.5f, Carpan * -0.5f, 5);
+                newChip.transform.localRotation = Quaternion.Euler(-90, 0, -180);
+            }
+            else
+            {
+                var rndChip = Random.Range(0, Chips.Count);
+                var newChip = Instantiate(Chips[rndChip], transform.position, Quaternion.identity, transform);
+                newChip.transform.localPosition = new Vector3(-0.5f, ((i) + Carpan) * Height, 5);
+                newChip.transform.localRotation = Quaternion.Euler(-90, 0, -180);
+            }
+
+
+        }
+    }
+
+    public void SpawnChip(int _count)
+    {
+        for (int i = 0; i < _count; i++)
+        {
+
+            var rndChip = Random.Range(0, Chips.Count);
+            var newChip = Instantiate(Chips[rndChip], transform.position, Quaternion.identity, transform);
+            MyMoneys.Add(newChip.GetComponent<Money>());
+            newChip.transform.localPosition = new Vector3(-0.5f, ((MyMoneys.Count - 1) + Carpan) * Height, 5f);
+            newChip.transform.localRotation = Quaternion.Euler(270, 0, -180);
+        }
+        MoneyTextWrite();
+    }
+    public void RemoveChip(int _count)
+    {
+        if (_count > MyMoneys.Count)
+        {
+            _count = MyMoneys.Count;
+        }
+        for (int i = 0; i < _count; i++)
+        {
+            Destroy(MyMoneys[MyMoneys.Count - 1].gameObject);
+            MyMoneys.Remove(MyMoneys[MyMoneys.Count - 1]);
+        }
+        MoneyTextWrite();
+    }
+    private void ResetScale()
+    {
+        foreach (var item in MyMoneys)
+        {
+            item.transform.DOScale(new Vector3(400, 400, 50), 0.01f);
+        }
+    }
+
+    public void MoneyTextWrite()
+    {
+        MyMoneyText.text = "$" + (MyMoneys.Count * 10f).ToString();
+    }
+}
